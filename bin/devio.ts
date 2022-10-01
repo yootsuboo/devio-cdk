650823495227c57fbf78c5cb22d460d0d6608421
ootsubo1192 <ootubo_stock@yahoo.co.jp>
2022-10-02 08:09:18 +0900

    create a vpc

--- bin/devio.ts
+++ bin/devio.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
+import * as cdk from '@aws-cdk/core';
 import { DevioStack } from '../lib/devio-stack';
 
 const app = new cdk.App();
--- lib/devio-stack.ts
+++ lib/devio-stack.ts
@@ -1,16 +1,14 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from '@aws-cdk/core';
+import { CfnVPC } from "@aws-cdk/aws-ec2";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class DevioStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct , id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'DevioQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    
+    new CfnVPC(this, "Vpc", {
+      cidrBlock: "10.0.0.0/16",
+      tags: [{ key: "Name", value: "devio-stg-vpc"}],
+    });
   }
 }
