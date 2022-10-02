2e9e02b54054ff4d8a618b0c3a612297a06e359b
ootsubo1192 <ootubo_stock@yahoo.co.jp>
2022-10-02 10:46:12 +0900

    Install Test-Framwork

--- test/devio.test.ts
+++ test/devio.test.ts
@@ -1,17 +1,16 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Devio from '../lib/devio-stack';
+import { expect, countResources, haveResources } from "@aws-cdk/assert";
+import * as cdk from "@aws-cdk/core";
+import * as Devio from "../lib/devio-stack";
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/devio-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new Devio.DevioStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test("Vpc", () => {
+  const app = new cdk.App();
+  const stack = new Devio.DevioStack(app, "DevioStack");
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  expect(stack).to(countResources("AWS::EC2::VPC", 1));
+  expect(stack).to(
+    haveResources("AWS::EC2::VPC", {
+      CidrBlock: "10.0.0.0/16",
+      Tags: [{ Key: "Name", Value: "devio-stg-vpc" }],
+    })
+  );
 });
